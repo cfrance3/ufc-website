d3bb022adce499801ea0f25ae8947d18b9ce744e
Caden France <160537486+cfrance3@users.noreply.github.com>
2026-01-14 14:01:57 -0600

    Implemented basic frontend with pages for home, fighters, fights, events, and search.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,54 @@
-from fastapi import FastAPI
-from backend.app.routers import fighters, events, fights
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from backend.app.routers import fighters, events, fights, search
+
 
 app = FastAPI(title="UFC Database API")
+
+templates = Jinja2Templates(directory="frontend/templates")
+
+app.mount(
+    "/static",
+    StaticFiles(directory="frontend/static"),
+    name="static"
+)
 
 app.include_router(fighters.router)
 app.include_router(events.router)
 app.include_router(fights.router)
 
 @app.get("/")
-def root():
-    return {"status": "UFC API running"}
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+@app.get("/search")
+def search(request: Request, q: str = None):
+    return templates.TemplateResponse(
+        "search/index.html",
+        {"request": request, "query": q}
+    )
+
+@app.get("/fights")
+def fights(request: Request, q: str = None):
+    return templates.TemplateResponse(
+        "fights/list.html",
+        {"request": request, "query": q}
+    )
+
+@app.get("/fighters")
+def fights(request: Request, q: str = None):
+    return templates.TemplateResponse(
+        "fighters/list.html",
+        {"request": request, "query": q}
+    )
+
+@app.get("/events")
+def fights(request: Request, q: str = None):
+    return templates.TemplateResponse(
+        "events/list.html",
+        {"request": request, "query": q}
+    )
